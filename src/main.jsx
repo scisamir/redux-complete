293839d471ad9dfd8473575a97b8a586f35c379f
Samir Idris <samzidris11@gmail.com>
2024-08-07 03:17:00 +0100

    async logic, thunks

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import './index.css'
 import { store } from './app/store'
 import { Provider } from 'react-redux'
+import { fetchUsers } from './features/users/usersSlice.js'
+
+store.dispatch(fetchUsers())
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
